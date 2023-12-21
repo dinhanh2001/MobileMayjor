ce5a2db3cee710c97eed8f1def02a3fc066cf5fb
AnhGoDev <devbe1@tasvietnam.com>
2023-12-21 22:44:11 +0700

    BACKUP finally

---
+++ app/screens/index.ts
@@ -0,0 +1,19 @@
+export * from "./WelcomeScreen"
+// @demo remove-block-start
+export * from "./LoginScreen"
+// export * from "./DemoCommunityScreen"
+
+// @demo remove-block-end
+// export other screens here
+export * from "./HomeScreen"
+export * from "./QrScreen"
+export * from "./SettingScreen"
+export * from "./Explain"
+export * from "./AttendanceScreen"
+export * from "./StaffExplainListScreen"
+export * from "./ExplainDetailScreen"
+export * from "./ApprovalScreen"
+export * from "./ChangeInforScreen"
+export * from "./NotificationScreen"
+export * from "./ChangePasswordScreen"
+export * from "./NotificationDetailScreen"
